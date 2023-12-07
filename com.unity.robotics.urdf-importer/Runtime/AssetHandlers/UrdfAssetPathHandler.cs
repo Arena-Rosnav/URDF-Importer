9a948642f6cb95c115eb47aa052cda597eec031c
bowski23 <golebiowski@campus.tu-berlin.de>
2023-12-07 14:24:55 +0100

    Temp Workaround for Path Problems

--- com.unity.robotics.urdf-importer/Runtime/AssetHandlers/UrdfAssetPathHandler.cs
+++ com.unity.robotics.urdf-importer/Runtime/AssetHandlers/UrdfAssetPathHandler.cs
@@ -16,6 +16,11 @@ using System.IO;
 using UnityEngine;
 using System;
 using System.Diagnostics;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Linq;
+using System.Runtime.InteropServices;
+using Codice.Client.BaseCommands.Triggers;
 
 namespace Unity.Robotics.UrdfImporter
 {
@@ -24,6 +29,46 @@ namespace Unity.Robotics.UrdfImporter
         //Relative to Assets folder
         private static string packageRoot;
         private const string MaterialFolderName = "Materials";
+
+        private static string OsPathSeparator
+        {
+            get
+            {
+
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    return ";";
+                }
+                return ":";
+            }
+        }
+
+        public static IEnumerable<string> RosPackagePaths
+        {
+            get
+            {
+                String ros_paths = System.Environment.GetEnvironmentVariable("ROS_PACKAGE_PATH");
+
+                IEnumerable<string> paths;
+
+                if (ros_paths != null)
+                {
+                    return ros_paths.Split(OsPathSeparator);
+                }
+                
+                // workaround for now if the workspace hasn't been sourced for the executable, e.g. launching from Unity Hub while testing
+                // should probalby use regex that includes different directory seperators to stay OS agnostic
+                // also requires arena-unity to be in the arena_ws
+                // Todo: make fallback paths configurable
+                paths = new List<string> { "/opt/ros/noetic/share/" };
+                string dirPath = System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+                if (dirPath.Contains("src/arena-unity"))
+                {
+                    return paths.Append(dirPath[..dirPath.IndexOf("arena-unity")]);
+                }
+                return paths;
+            }
+        }
 
         #region SetAssetRootFolder
         public static void SetPackageRoot(string newPath, bool correctingIncorrectPackageRoot = false)
@@ -49,7 +94,7 @@ namespace Unity.Robotics.UrdfImporter
         {
             return packageRoot;
         }
-        
+
         public static string GetRelativeAssetPath(string absolutePath)
         {
             string assetPath = absolutePath;
@@ -66,7 +111,7 @@ namespace Unity.Robotics.UrdfImporter
                 }
 #endif
             }
-            else 
+            else
             {
                 assetPath = "Assets" + absolutePath.Substring(Application.dataPath.Length);
             }
@@ -80,29 +125,37 @@ namespace Unity.Robotics.UrdfImporter
             {
                 fullPath += relativePath.Substring("Assets".Length);
             }
-            else 
+            else
             {
                 fullPath = fullPath.Substring(0, fullPath.Length - "Assets".Length) + relativePath;
             }
             return fullPath.SetSeparatorChar();
         }
 
-        public static string GetRelativeAssetPathFromUrdfPath(string urdfPath, bool convertToPrefab=true)
+        public static string GetRelativeAssetPathFromUrdfPath(string urdfPath, bool convertToPrefab = true)
         {
             string path;
             bool useFileUri = false;
             if (!urdfPath.StartsWith(@"file://") && !urdfPath.StartsWith(@"package://"))
             {
-               if (urdfPath.Substring(0, 3) == "../")
+                if (urdfPath.Substring(0, 3) == "../")
                 {
-                   UnityEngine.Debug.LogWarning("Attempting to replace file path's starting instance of `../` with standard package notation `package://` to prevent manual path traversal at root of directory!");
-                   urdfPath = $@"package://{urdfPath.Substring(3)}";
+                    UnityEngine.Debug.LogWarning("Attempting to replace file path's starting instance of `../` with standard package notation `package://` to prevent manual path traversal at root of directory!");
+                    urdfPath = $@"package://{urdfPath.Substring(3)}";
                 }
             }
             // loading assets relative path from ROS/ROS2 package.
             if (urdfPath.StartsWith(@"package://"))
             {
                 path = urdfPath.Substring(10).SetSeparatorChar();
+
+                //search through ROS package directories, if not found in unity repo
+                if(!IsValidAssetPath(path)){
+                    foreach(var packagePath in RosPackagePaths){
+                        if(IsValidAssetPath(packagePath+path))
+                            return packagePath+path;
+                    }
+                }
             }
             // loading assets from file:// type URI.
             else if (urdfPath.StartsWith(@"file://"))
@@ -115,13 +168,14 @@ namespace Unity.Robotics.UrdfImporter
                 path = urdfPath.SetSeparatorChar();
             }
 
-            if (convertToPrefab) 
+            if (convertToPrefab)
             {
                 if (Path.GetExtension(path)?.ToLowerInvariant() == ".stl")
                     path = path.Substring(0, path.Length - 3) + "prefab";
 
             }
-            if (useFileUri) {
+            if (useFileUri)
+            {
                 return path;
             }
             return Path.Combine(packageRoot, path);
@@ -137,7 +191,7 @@ namespace Unity.Robotics.UrdfImporter
             }
 #endif
             //RuntimeImporter. TODO: check if the path really exists
-            return true;
+            return Directory.Exists(path) || File.Exists(path);
         }
 
         #region Materials
